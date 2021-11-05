bb7738b92c6896c6e9b1abf5e5782a2d95823e3c
ChrisLora <chrisliden218@gmail.com>
2021-11-05 04:23:37 -0400

    finished withdraw, deposit, and validatePin functions, and exported all te atm functions

--- atm.js
+++ atm.js
@@ -3,25 +3,39 @@
 const { pin, bal } = require("./account");
 
 function getBalance() {
-  //TODO: Return the customer's acct. balance
+  //***DONE***TODO: Return the customer's acct. balance
   return bal;
 }
 
 function withdraw(withdrawAmount) {
-  //TODO: withdraw amount from current acct. balance
+  //***DONE***TODO: withdraw amount from current acct. balance
+  let newBalance = bal - withdrawAmount;
+
   // Log the current balance after withdrawal is made
+  console.log(newBalance);
+  return newBalance;
 }
 
 function deposit(depositAmount) {
-  //TODO: deposit amount to current acct. balance
+  //***DONE***TODO: deposit amount to current acct. balance
+let newBalance = bal + depositAmount;
+
   // Log the current balance after deposit is made
+  console.log(newBalance);
+  return newBalance;
 }
 
 function validatePin(enteredPin) {
-  //TODO: Check if entered pin matches account.js pin
-  //Allow access to ATM if matching
-  //Return value should be a Boolean (true or false)
-  //In English: if the PIN entered is correct, return True. Otherwise, return False.
+  //***DONE***TODO: Check if entered pin matches account.js pin/Allow access to ATM if matching/Return value should be a Boolean (true or false)/In English: if the PIN entered is correct, return True. Otherwise, return False.
+  if (enteredPin = pin){
+    return true;
+  }
 }
 
-//TODO: Export these functions
+//***DONE***TODO: Export these functions
+module.exports = {
+  getBalance: getBalance,
+  withdraw: withdraw,
+  deposit: deposit,
+  validatePin: validatePin,
+}
