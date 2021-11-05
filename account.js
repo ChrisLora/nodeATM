627a7f717c61c873936aa6172dd5b1099630f8b4
ChrisLora <chrisliden218@gmail.com>
2021-11-05 04:07:32 -0400

    created account pin and balance variables and exported from account.js

--- account.js
+++ account.js
@@ -2,6 +2,12 @@
 //! START HERE! Then atm.js, then index.js
 //! Remember to run npm init to create your Node application!
 
-//TODO: Create variables for account balance & account pin
+//***DONE***TODO: Create variables for account balance & account pin 
+const accountPin = 1234
+let balance = 100
 
 //Export these variables
+module.exports = {
+    pin: accountPin,
+    bal: balance
+}
